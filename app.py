09f41bfc2303760ac97c7fadd3fec2351e445f88
pionero-dev-linhnd <linh.nguyen@pionero.io>
2024-10-01 11:34:44 +0700

    x

--- app.py
+++ app.py
@@ -1,43 +1,26 @@
 import os
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from yt_dlp import YoutubeDL
 from pydantic import BaseModel
-import http.cookiejar
-import urllib.parse 
+import urllib.parse  # Thêm import này
 
 app = FastAPI()
 
 class VideoRequest(BaseModel):
     video_url: str
 
-def save_cookies_from_string(cookies_string: str, cookies_file: str):
-
+def save_cookies(cookies_string: str, cookies_file: str):
+    # Giải mã cookies
     decoded_cookies = urllib.parse.unquote(cookies_string)
-
-    jar = http.cookiejar.CookieJar()
-    
-    # Phân tách cookies
-    cookies_list = decoded_cookies.split('; ')
-    for cookie in cookies_list:
-        name, value = cookie.split('=', 1)
-        # Tạo cookie và thêm vào cookiejar
-        jar.set_cookie(http.cookiejar.Cookie(version=0, name=name, value=value,
-                                             port=None, port_specified=False,
-                                             domain='youtube.com', domain_specified=True,
-                                             domain_initial_dot=False, path='/', 
-                                             path_specified=True, secure=False, 
-                                             expires=None, discard=True, comment=None, 
-                                             comment_url=None, rest=None))
     
     # Lưu cookies vào tệp
     with open(cookies_file, 'w') as f:
-        for cookie in jar:
-            f.write(f"{cookie.name}={cookie.value}\n")
+        f.write(decoded_cookies)
 
 def get_download_url(video_url: str, cookies: str) -> str:
     cookies_file = 'cookies.txt'
-    save_cookies_from_string(cookies, cookies_file)
+    save_cookies(cookies, cookies_file)
 
     ydl_opts = {
         'format': 'm4a/bestaudio/best',
@@ -53,12 +36,13 @@ def get_download_url(video_url: str, cookies: str) -> str:
 @app.post("/get_video_url/")
 def get_video_url(request: VideoRequest, req: Request):
     cookies_header = req.headers.get('cookies')
-    print(cookies_header)
+    if not cookies_header:
+        raise HTTPException(status_code=400, detail="No cookies provided")
 
     download_url = get_download_url(request.video_url, cookies_header)
     if download_url:
         return {"download_url": download_url}
-    return {"error": "Failed to retrieve the video download URL"}
+    raise HTTPException(status_code=404, detail="Failed to retrieve the video download URL")
 
 # Main block to run the server
 if __name__ == "__main__":
